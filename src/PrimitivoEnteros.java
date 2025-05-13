25b91011bf02e8d4635e6f78edb8bd29677ce612
Fernando Rodríguez Pérez <fernandocorreo79@gmail.com>
2025-05-13 12:16:23 +0100

    Primer commit para el primer programa de clase de big data

--- src/PrimitivoEnteros.java
+++ src/PrimitivoEnteros.java
@@ -1,2 +1,11 @@
 public class PrimitivoEnteros {
+
+    public static void main(String[] args) {
+        byte numeroByte = 127;
+        short numeroShort =3267;
+        int numeroInt = 2147483647;
+        long numeroLong = 9223372036854775807L;
+        var numeroVar = 9223372036854775808f;
+    }
+
 }
