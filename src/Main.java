3bee7ed2907bf923b3338ca387c4ebfa3b3311c4
Fernando Rodríguez Pérez <fernandocorreo79@gmail.com>
2025-05-13 11:25:58 +0100

    Primer commit para el primer programa de clase de big data

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+String saludar = "Hola Pepito";
+
+        System.out.println(saludar.toUpperCase());// Saludar con método
+        /*
+        comentario
+
+         */
+        int numero = 11;
+        int numero1;
+        System.out.println(numero);
+        System.out.println(numero1=10);
+
+        boolean valor = false;
+
+        if(valor){
+
+            System.out.println(numero);
+        }else{
+
+            System.out.println(numero1);
+        }
+
+    }
+
+}
